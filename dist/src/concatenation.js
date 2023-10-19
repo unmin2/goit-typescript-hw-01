b49be58a966f8adc23394d6e8633a4a334843477
unmin2 <125081601+unmin2@users.noreply.github.com>
2023-10-19 15:02:09 +0300

    change

--- dist/src/concatenation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.concatenation = void 0;
-function concatenation(firstWord, secondWord) {
-    console.log(`${firstWord} ${secondWord}`);
-}
-exports.concatenation = concatenation;
-//# sourceMappingURL=concatenation.js.map
--- dist/test.js
+++
@@ -1,5 +0,0 @@
-function add(num1, num2) {
-    return num1 + num2;
-}
-add(1, 1);
-//# sourceMappingURL=test.js.map
